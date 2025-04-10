1ffb654d5186993f1ad7a942f690eaa15891baec
Simranjot Kaur <simranbrar0412@gmail.com>
2025-04-09 23:44:51 -0600

    Add unit test for simran's name

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Simran on the page', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const nameEl = screen.getByText(/Simranjot Kaur Brar/i);
+  expect(nameEl).toBeInTheDocument();
 });
